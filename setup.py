d4ffdf0b386656dbac5c42321f85dd83cd05772e
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-17 21:25:58 +0100

    Add a LICENSE and update a setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="scapy_helper",
-    author="NexSabre",
-    description="Few functions which make a scapy easy",
+    description="Several features that should help you use Scapy",
+    author="Nex Sabre",
+    author_email="nexsabre@protonmail.com",
     version="0.1",
-    url="",
+    url="https://github.com/NexSabre/scapy_helper",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=["scapy"]
 )
