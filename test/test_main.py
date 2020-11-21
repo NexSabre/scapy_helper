69059af47dd4294f09bb32a601a21a6b2f617db8
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-21 13:28:31 +0100

    Correct name of test class

--- test/test_main.py
+++ test/test_main.py
@@ -2,19 +2,20 @@ from unittest import TestCase
 
 from scapy.layers.l2 import Ether
 
-from scapy_helper import get_hex, show_hex, show_diff
+from scapy_helper import get_hex, show_diff
 from scapy_helper.main import _diff
 
 
-class Test_Scapy_Helper(TestCase):
+class TestScapyHelper(TestCase):
     def setUp(self) -> None:
-        self.ether = Ether()
+        self.ether = Ether(src="ff:ff:ff:ff:ff:ff",
+                           dst="00:00:00:00:00:00")
 
     def test_get_hex(self):
-        assert get_hex(self.ether) == "ff ff ff ff ff ff e0 d5 5e e6 6c 8e 90 00"
+        assert get_hex(self.ether) == "ff ff ff ff ff ff 00 00 00 00 00 00 90 00"
 
     def test_show_diff(self):
-        ether_wrong = "ff ff fc ff ff fa e0 d5 5e e6 6c 8e 90 00"
+        ether_wrong = "ff ff ff ff ff ff 00 00 00 00 00 00 90 00"
         first_row, second_row, status = _diff(self.ether, ether_wrong)
         self.assertEqual(first_row,
                          ['__', '__', 'ff', '__', '__', 'ff', '__', '__', '__', '__', '__', '__', '__', '__'])
@@ -23,18 +24,18 @@ class Test_Scapy_Helper(TestCase):
         self.assertIsNotNone(status)
 
     def test_show_more_diff(self):
-        show_diff(self.ether, "ff ff ff ff ff ff e0 d5 5e e6 6c 8e 90 00")
+        show_diff(self.ether, "ff ff ff ff ff ff 00 00 00 00 00 00 90 00")
 
-    def test_diff_packets_are_same_len(self):
+    def test_diff_should_return_3_elements(self):
         too_long_ether = get_hex(Ether() / "additional string")
 
-        _diff(self.ether, too_long_ether)
+        diff_tuple = _diff(self.ether, too_long_ether)
+        self.assertEqual(len(diff_tuple), 3, "_diff should return only a 3 elements")
 
     def test_diff_should_return_same_len_list_if_one_of_them_was_shorter(self):
         too_long_ether = get_hex(Ether() / "additional string")
 
         diff_tuple = _diff(self.ether, too_long_ether)
-        self.assertEqual(len(diff_tuple), 3, "_diff should return only a 3 elements")
         self.assertEqual(len(diff_tuple[0]), len(diff_tuple[1]), "List should be the same length")
 
         self.assertNotEqual(
