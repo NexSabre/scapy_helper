5e519e02e93e279480d1cdb9b9efa1c981520d61
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-21 16:29:55 +0100

    "Unlock" requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Several features that should help you use Scapy",
     author="Nex Sabre",
     author_email="nexsabre@protonmail.com",
-    version="0.1.7",
+    version="0.1.8",
     url="https://github.com/NexSabre/scapy_helper",
     packages=find_packages(),
     classifiers=[
@@ -13,5 +13,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["scapy==2.4.4", "tabulate==0.8.7"]
+    install_requires=["scapy~=2.4.4", "tabulate~=0.8.7"]
 )
