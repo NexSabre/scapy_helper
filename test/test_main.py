b6fd72e80ca21ea5be5e3dd12b1806f162e3b1e0
Nex Sabre <NexSabre@users.noreply.github.com>
2020-12-08 20:53:42 +0100

    Change _diff > diff and add small refactor

--- test/test_main.py
+++ test/test_main.py
@@ -4,13 +4,38 @@ from scapy.layers.l2 import Ether
 from scapy.utils import chexdump
 
 from scapy_helper import get_hex, show_diff
-from scapy_helper.main import _diff, hex_equal
+from scapy_helper.main import diff, hex_equal
 
 
 class TestScapyHelper(TestCase):
     def setUp(self) -> None:
         self.ether = Ether(dst="ff:ff:ff:ff:ff:ff",
                            src="00:00:00:00:00:00")
+
+    def test_diff_list_equal(self):
+        result = diff("ff ff ff ff ff ff 00 00 00 00 00 00 90 00", "ff ff ff ff ff ff 00 00 00 00 00 00 90 00")
+        self.assertEqual(
+            result,
+            (['__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+             ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__', '__'], []),
+            "Results should be equal"
+        )
+
+    def test_diff_list_should_be_diff_at_6_position(self):
+        result = diff("ff ff ff ff ff ff 11 00 00 00 00 00 90 00", "ff ff ff ff ff ff 00 00 00 00 00 00 90 00")
+        self.assertEqual(
+            result,
+            (['__', '__', '__', '__', '__', '__', '11', '__', '__', '__', '__', '__', '__', '__'],
+             ['__', '__', '__', '__', '__', '__', '00', '__', '__', '__', '__', '__', '__', '__'], [6]),
+            "Results should be equal"
+        )
+
+    def test_diff_refactor(self):
+        with self.assertRaises(NotImplementedError):
+            diff("00 11", "00 11", "00 00")
+
+        with self.assertRaises(NotImplementedError):
+            diff("00 11")
 
     def test_get_hex(self):
         assert get_hex(self.ether) == "ff ff ff ff ff ff 00 00 00 00 00 00 90 00"
@@ -23,7 +48,7 @@ class TestScapyHelper(TestCase):
 
     def test_show_diff(self):
         ether_wrong = "ff ff ff ff ff ff 00 00 00 00 aa 00 90 00"
-        first_row, second_row, status = _diff(self.ether, ether_wrong)
+        first_row, second_row, status = diff(self.ether, ether_wrong)
 
         self.assertEqual(first_row,
                          "__ __ __ __ __ __ __ __ __ __ 00 __ __ __".split())
@@ -33,7 +58,7 @@ class TestScapyHelper(TestCase):
         self.assertEqual(status, [10, ])
 
     def test_show_diff_for_same_elements(self):
-        first_row, second_row, status = _diff(self.ether, self.ether)
+        first_row, second_row, status = diff(self.ether, self.ether)
 
         self.assertEqual(first_row,
                          "__ __ __ __ __ __ __ __ __ __ __ __ __ __".split())
@@ -47,13 +72,13 @@ class TestScapyHelper(TestCase):
     def test_diff_should_return_3_elements(self):
         too_long_ether = get_hex(Ether() / "additional string")
 
-        diff_tuple = _diff(self.ether, too_long_ether)
+        diff_tuple = diff(self.ether, too_long_ether)
         self.assertEqual(len(diff_tuple), 3, "_diff should return only a 3 elements")
 
     def test_diff_should_return_same_len_list_if_one_of_them_was_shorter(self):
         too_long_ether = get_hex(Ether() / "additional string")
 
-        diff_tuple = _diff(self.ether, too_long_ether)
+        diff_tuple = diff(self.ether, too_long_ether)
         self.assertEqual(len(diff_tuple[0]), len(diff_tuple[1]), "List should be the same length")
 
         self.assertNotEqual(
