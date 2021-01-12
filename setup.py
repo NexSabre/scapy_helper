5de65a4492d563cbab1059fa0c5c9d41cf93aa5a
Nex Sabre <NexSabre@users.noreply.github.com>
2021-01-12 21:21:06 +0100

    Initial commit for the mirrage package

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     license='MIT',
     install_requires=["tabulate~=0.8.7"]
