2ffa635678399fa2a1554defe6fd667657a0d1f1
Nex Sabre <NexSabre@users.noreply.github.com>
2021-01-14 21:56:08 +0100

    Resolve issue around Py2&Py3 hexdump

--- scapy_helper/hexdump.py
+++ scapy_helper/hexdump.py
@@ -17,9 +17,9 @@ def hexdump(packet, dump=False, to_list=False):
                 for start in range(0, len(obj), num)
                 if obj[start:start + num]]
 
-    def __process_hexdump():
+    def __process_hexdump(ppacket):
         row = []
-        for i, line in enumerate(split(processed_packet, 16)):
+        for i, line in enumerate(split(ppacket, 16)):
             console_char = [to_char(int(x, 16)) for x in line]
             if len(line) < 16:
                 line += ["  " for _ in range(16 - len(line))]
@@ -28,9 +28,9 @@ def hexdump(packet, dump=False, to_list=False):
                        (i, ' '.join(line), ''.join(console_char)))
         return row
 
-    def __alternative_process_hexdump():
+    def __alternative_process_hexdump(ppacket):
         row = []
-        for i, line in enumerate(split(processed_packet, 16)):
+        for i, line in enumerate(split(ppacket, 16)):
             console_char = [to_char(x) for x in line]
             if len(line) < 16:
                 line += ["  " for _ in range(16 - len(line))]
@@ -43,11 +43,14 @@ def hexdump(packet, dump=False, to_list=False):
         processed_packet = _prepare(packet)
         if not processed_packet:
             return
-
-        rows = __process_hexdump()
+        rows = __process_hexdump(processed_packet)
     except ValueError:
+        # It's not a ideal workaround for detecting a proper
+        # format of input, but works on Py2 & Py3
         processed_packet = get_hex(packet)
-        rows = __alternative_process_hexdump()
+        if not processed_packet:
+            return
+        rows = __alternative_process_hexdump(processed_packet)
     if dump:
         if to_list:
             return rows
