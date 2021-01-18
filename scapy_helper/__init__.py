3d0118f01477b1ef580ecb57f87838421df08600
Nex Sabre <NexSabre@users.noreply.github.com>
2021-01-18 17:26:36 +0100

    Add a new functionalities into __init__.py

--- scapy_helper/__init__.py
+++ scapy_helper/__init__.py
@@ -3,4 +3,6 @@ from scapy_helper.hexdump import hexdump
 from scapy_helper.chexdump import chexdump
 from scapy_helper.compare import Compare
 
+from scapy_helper.helpers.mac import mac2int, int2mac
+
 from scapy_helper.test_case_extensions.packet_assert import PacketAssert
