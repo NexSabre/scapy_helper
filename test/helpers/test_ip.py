51b0170013668bc9689fab82ed3332726e613bea
Nex Sabre <NexSabre@users.noreply.github.com>
2021-01-18 22:06:19 +0100

    Add a quick conversion of ip -> int & int -> ip.
    Functionalities should be more helpful for the Python 2 users, since the Python 3 is more useful to use ipaddress lib

---
+++ test/helpers/test_ip.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from scapy_helper.helpers.ip import int2ip, ip2int
+
+
+class TestIP(TestCase):
+    def setUp(self):
+        self.integer = 3232235521
+        self.ip = "192.168.0.1"
+
+    def test_ip2int(self):
+        self.assertEqual(
+            ip2int(self.ip),
+            self.integer,
+            "Values should be equal"
+        )
+
+    def test_int2ip(self):
+        self.assertEqual(
+            int2ip(self.integer),
+            self.ip,
+            "Values should be equal"
+        )
