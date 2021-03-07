158facc91c0958588a4446ae697317de70e5c8aa
Nex Sabre <NexSabre@users.noreply.github.com>
2021-03-07 19:31:56 +0100

    Add a diff function to the __init__.py

--- scapy_helper/__init__.py
+++ scapy_helper/__init__.py
@@ -3,5 +3,5 @@ from scapy_helper.compare import Compare
 from scapy_helper.helpers.ip import ip2int, int2ip
 from scapy_helper.helpers.mac import mac2int, int2mac
 from scapy_helper.hexdump import hexdump
-from scapy_helper.main import get_hex, show_diff, show_hex, table, hex_equal
+from scapy_helper.main import get_hex, show_diff, show_hex, table, hex_equal, diff
 from scapy_helper.test_case_extensions.packet_assert import PacketAssert
